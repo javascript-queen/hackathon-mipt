e770d66b999f7802d04253268241899981981898
Anna Traylor <jsqueen.design@gmail.com>
2024-04-13 07:13:11 +0300

    feat: 25 symbols

--- main/models/doc.py
+++ main/models/doc.py
@@ -3,7 +3,7 @@ from django.db import models
 from .file import File
 from .user import User
 
-
+# todo: ограничение в 25 символов
 class Doc(models.Model):
     user = models.ForeignKey(User, related_name='docs', on_delete=models.CASCADE)
     file = models.ForeignKey(File, related_name='docs', on_delete=models.CASCADE)
