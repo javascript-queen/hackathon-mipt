3223229c83ced498d75facd26db6e4dbc0ef6866
Yuri <anisimov.ius@phystech.edu>
2024-04-14 03:45:11 +0300

    validate comparison docs belong to the same user

--- main/serializers/comparison.py
+++ main/serializers/comparison.py
@@ -16,10 +16,19 @@ class ComparisonSerializer(HyperlinkedModelSerializer):
         requirements = data.get('requirements')
         application = data.get('application')
         errors = {}
+
+        # Ensure one doc - requirements, the other - application.
         if not requirements.is_requirements:
             errors['requirements'] = f"Документ {requirements.id} - не требования, а заявка."
         if application.is_requirements:
             errors['application'] = f"Документ {application.id} - не заявка, а требования."
+
+        # Ensure both docs belong to the same user
+        if application.user.id == requirements.user.id:
+            # note: ['error msg1', ...] is also ok
+            errors['non_field_errors'] = f'Документы принадлежат разным пользователям.'
+
         if errors:
             raise serializers.ValidationError(errors)
+
         return data
